8fc1f50519cf0516e8baedd9f95beb5a317d9c2b
Илья Снежко <valliv2007@ya.ru>
2022-12-22 16:18:16 +0200

    created app recipes

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'api',
+    'recipes',
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist'

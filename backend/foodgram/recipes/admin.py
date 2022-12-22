031f55285a627e680a1a9df798c97ff34e6713af
Илья Снежко <valliv2007@ya.ru>
2022-12-22 17:10:17 +0200

    created model Ingredient

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Ingredient
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_filter = ('name',)
+    list_display = ('name', 'measurement_unit')
--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    """Модель ингридиентов"""
+    name = models.CharField('Ingredient_name', blank=False, max_length=150)
+    measurement_unit = models.CharField('Measurement_unit', max_length=150)
+
+    class Meta:
+        verbose_name = 'Ingredient'
+        verbose_name_plural = 'Ingredients'
+
+    def __str__(self):
+        return self.name

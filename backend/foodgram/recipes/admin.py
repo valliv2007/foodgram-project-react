41e9e7ff8f3677d951867f4ac848dfd2ecf08db3
Илья Снежко <valliv2007@ya.ru>
2022-12-30 00:09:58 +0200

    add inlines models and favorite count in admin

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -4,6 +4,15 @@ from .models import (Cart, Favorite, Ingredient, IngredientRecipe,
                      Recipe, Tag, TagRecipe)
 
 
+class IngredientRecipeInline(admin.TabularInline):
+    model = IngredientRecipe
+
+
+class TagRecipeInline(admin.TabularInline):
+    model = TagRecipe
+    extra = 2
+
+
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
     list_filter = ('name',)
@@ -14,10 +23,20 @@ class IngredientAdmin(admin.ModelAdmin):
 class RecipeAdmin(admin.ModelAdmin):
     list_filter = ('name', 'author', 'tags')
     list_display = ('name', 'author')
+    inlines = (IngredientRecipeInline, TagRecipeInline)
+    fieldsets = (
+        ('Main params', {'fields': (
+            'name', 'author', 'image', 'text', 'cooking_time')}),
+        ('Added to favorite', {'fields': ('favorite_count',)})
+        )
+    readonly_fields = ('favorite_count',)
+
+    def favorite_count(self, obj):
+        return obj.favorites.count()
 
 
 admin.site.register(Tag)
 admin.site.register(IngredientRecipe)
 admin.site.register(TagRecipe)
-admin.site.register(Favorite)
 admin.site.register(Cart)
+admin.site.register(Favorite)
